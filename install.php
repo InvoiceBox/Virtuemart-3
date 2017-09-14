0f85d9cc08978317f3caf80fbb59ec5c3a026383
zikkuratvk <zikkuratvk@gmail.com>
2017-09-14 16:03:10 +0200

    fix установки

--- install.php
+++ install.php
@@ -10,19 +10,8 @@ class plgVMPaymentInvoiceboxInstallerScript {
         $path = JPATH_ROOT . DS . 'plugins' . DS . 'vmpayment' . DS . 'invoicebox' . DS . 'install' . DS;
         $app  = JFactory::getApplication();
         
-        if (!JFolder::move($path . 'helpers', JPATH_ROOT . DS . 'libraries' . DS . 'invoicebox')) {
-            $app->enqueueMessage('Couldnt move file');
-        }
-
-        if (!JFile::move($path . 'fail.php', JPATH_ROOT . DS . 'fail.php')) {
-            $app->enqueueMessage('Couldnt move file');
-        }
-
-        if (!JFile::move($path . 'success.php', JPATH_ROOT . DS . 'success.php')) {
-            $app->enqueueMessage('Couldnt move file');
-        }
-
-        if (!JFile::move($path . 'invoicebox.gif', JPATH_ROOT . DS . 'images' . DS . 'invoicebox.png')) {
+       
+        if (!JFile::move($path . 'invoicebox.png', JPATH_ROOT . DS . 'images' . DS . 'invoicebox.png')) {
             $app->enqueueMessage('Couldnt move file');
         }        
         
